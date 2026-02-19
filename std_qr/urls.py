3c90941d91d5f18dbfcb461db904b502ac34d46c
jagadesh.alpha8 <jagadesh.alpha8@gmail.com>
2026-02-19 07:29:14 +0530

    iot_esp32_new

--- std_qr/urls.py
+++ std_qr/urls.py
@@ -9,11 +9,9 @@ urlpatterns = [
     path('cybersec-ibm/', registration_view, {'form_type': 'cybersec_ibm'}, name='cybersec-ibm-form'),
     path('ev-technology/', registration_view, {'form_type': 'ev_tech'}, name='ev-technology-form'),
     path('embedded-c-mc/', registration_view, {'form_type': 'embedded_c_mc'}, name='embedded-c-mc-form'),
-    path('iot-esp32/', registration_view, {'form_type': 'iot_esp32'}, name='iot-esp32-form'),
+    path('iot_esp32_new/', registration_view, {'form_type': 'iot_esp32'}, name='iot-esp32-form'),
     path('network-essentials/', registration_view, {'form_type': 'network_essentials'}, name='network-essentials-form'),
     path('ajax/load-districts/', load_districts, name='ajax_load_districts'),
     path('ajax/load-colleges/', load_colleges, name='ajax_load_colleges'),
     path('success/', registration_success, name='registration_success'),
-    
-
 ]
